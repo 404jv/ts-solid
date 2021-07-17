0014bb92731d948d1506c6f92a1abb74c4ef29d4
João Victor <joaovictorramalho7@gmail.com>
2021-07-17 20:24:05 -0300

    route to course controller

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,9 @@
 import express from 'express';
+import { controller } from './routes';
 
 const app = express();
+app.use(express.json());
+
+app.post('/', controller);
 
 app.listen(3333, () => console.log('🚀 Server is running at http://localhost:3333'));
